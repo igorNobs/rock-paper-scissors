15d6fa037120eeebca064eed0db290464438d30b
Igor Romanenko <igor@Terminator-2.local>
2021-03-07 22:11:59 -0500

    util funcs

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const gameState = {};
+
+function hide(el) {
+  el.classList.add('hidden');
+}
+
+function show(el) {
+  el.classList.remove('hidden');
+}
+
 (function() {
 
 })();
